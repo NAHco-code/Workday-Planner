f033d689692fff1dad7cface41d018dc20c9a790
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-02-26 15:13:00 -0600

    Function Build: implement timeblock classes - past, present, future
    Use jquery to select all rows in timeblocks, traverse to text area, define row hour + split class string to select hour; implement if else statements to add classes to timeblock rows based on current time of day. Re-work id's in html for js accessibility. Attempt to select last child of hours div to implement bottom border - failed.

--- Develop/script.js
+++ Develop/script.js
@@ -67,47 +67,56 @@ $(document).ready(function () {
 
 
 
-// //save to-do-items to local storage
+//save to-do-items to local storage
 // saveBtn.addEventListener('click', storeTask()){
     
 //     localStorage.setItem('task[]', 'taskText.stringify');
 // }
 
 
-// var tableRows = $('#row');
-// var tableBody = $('#container container-fluid');
-// $(function ()){
-//     $('#')
-// }
-// //var main area
+var tableRows = $('.row');
+tableRows.each(function (index) {
+    console.log($(this).children('textarea'))
+    let rowHour = $(this).attr('id').split('_')[1];
+    if (moment().hours() < rowHour) {
+        $(this).children('textarea').addClass('future')
+    }
+    else if (moment().hours() == rowHour) {
+        $(this).children('textarea').addClass('present')
+    }
+    else {
+        $(this).children('textarea').addClass('past')
+    }
+});
+//var main area
 
-//var hours object //array
-//var browser storage
+// var hours object //array
+// var browser storage
 
-//display date moments.js
+// display date moments.js
 
-//display time blocks
-//for loop to appends divs for time blocks
+// display time blocks
+// for loop to appends divs for time blocks
 
-//function color code time block to reflect time of day
-    //**first comparison doesn't work**
-    //let
-    //let
-    //for loop
-    //if else
+// function color code time block to reflect time of day
+//     **first comparison doesn't work**
+//     let
+//     let
+//     for loop
+//     if else
 
-//event listener to save input
-    //
-    //push appends item to end of array
-    //append saved items in same div to 
+// event listener to save input
+    
+//     push appends item to end of array
+//     append saved items in same div to 
 
 
-//write local storage items to correct row
-//if statement
-    //for loop
-        //let
-        //console.log
-        //
+// write local storage items to correct row
+// if statement
+//     for loop
+//         let
+//         console.log
+        
 
-        //when they click on the button, you want to save the value that's in the box, put it into an array (that you stringify ) when you put it back onto the page, you have to parse it. use get item
+//         when they click on the button, you want to save the value that's in the box, put it into an array (that you stringify ) when you put it back onto the page, you have to parse it. use get item
     

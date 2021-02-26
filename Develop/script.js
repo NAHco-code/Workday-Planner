5c428bb9418085fd119f8cee61aefb0ab7960e06
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-02-26 11:53:27 -0600

    Function Build: add timeblocks
    add time blocks using bootstrap box grid; style time blocks- padding on hours and save button to center text vertically, style save button hover; define time block object in js to allow save task function

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,45 @@
-//declare variables //
+// declare global //
+
+const savedTasksObj = [{
+    time: 9,
+    tasks: ['']    
+    },
+    {
+    time: 10,
+    tasks: [''] 
+    },
+    {
+    time: 11,
+    tasks: ['']
+    },
+    {
+    time: 12,
+    tasks: ['']    
+    },
+    {
+    time: 1,
+    tasks: ['']    
+    },
+    {
+    time: 2,
+    tasks: ['']    
+    },
+    {
+    time: 3,
+    tasks: ['']    
+    },
+    {
+    time: 4,
+    tasks: ['']    
+    },
+    {
+    time: 5,
+    tasks: ['']    
+    }
+];
+
+const currentDateTimeEL = $('#current_date_time');
 var dateTime; //empty date variable
-const currentDateTimeEL = $('#current_date_time'); //link to html
 
 //define function to live update current date and time
 var update = function () {
@@ -11,14 +50,36 @@ var update = function () {
 $(document).ready(function () {
     update();
     setInterval(update, 1000)
-});    
+});
+
+// const saveBtn = $('#save_btn');
+// const taskEl = $('.desription'); //append task items to DOM elements
+
+// const savedItem = '';
+    
+// function renderSaved(index) {
+//     let taskList = savedTasksObj[index].time + savedTasksObj[index].tasks;
+    
+//     let taskText = 
+//         '<textarea class="description col-8">' + savedTasksObj[index].tasks + '</textarea>';
+//     taskEl.innerHTML = taskText;
+// };
+
+
+
+// //save to-do-items to local storage
+// saveBtn.addEventListener('click', storeTask()){
+    
+//     localStorage.setItem('task[]', 'taskText.stringify');
+// }
+
 
 // var tableRows = $('#row');
 // var tableBody = $('#container container-fluid');
 // $(function ()){
 //     $('#')
 // }
-//var main area
+// //var main area
 
 //var hours object //array
 //var browser storage
@@ -47,4 +108,6 @@ $(document).ready(function () {
         //let
         //console.log
         //
+
+        //when they click on the button, you want to save the value that's in the box, put it into an array (that you stringify ) when you put it back onto the page, you have to parse it. use get item
     
